ac7e14a2a7f2f7cb71e0745a7f62329ed697859e
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-05-04 16:31:28 +0200

    Finalized project page

--- src/lib/components/project-card.svelte
+++ src/lib/components/project-card.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
- export let title: string;
- export let body: string;
+	export let title: string;
+	export let body: string;
+	export let url: string;
 </script>
 
-<div class="card w-auto md:w-96 lg:w-96 bg-base-300 shadow-xl">
+<div class="card w-auto bg-base-300 shadow-xl transition-all hover:scale-105">
 	<div class="card-body">
 		<h2 class="card-title">{title}</h2>
 		<p>{body}</p>
 		<div class="card-actions justify-start">
-			<a href="https://github.com/iamgabrieltv/IamChat">
+			<a href={url}>
 				<button class="btn btn-primary">GitHub</button>
 			</a>
 		</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,21 @@
 <script>
- import ProjectCard from "$lib/components/project-card.svelte";
+	import ProjectCard from '$lib/components/project-card.svelte';
 </script>
 
-<h1 class="text-3xl font-bold mb-3">Projects</h1>
+<h1 class="text-3xl font-bold">Projects</h1>
+<p class="text-lg mb-3">Here you can check out my projects on GitHub!</p>
 
-<div class="grid gap-1 auto-cols-auto">
-<ProjectCard title="Hello World!" body="Lorem Ipsum Dolor Sit Amet." />
-<ProjectCard title="Another Card!" body="Wow! It works" />
+<div class="divider" />
+
+<div class="grid gap-4 grid-cols-1 md:grid-cols-3">
+	<ProjectCard
+		title="IamBot"
+		body="A very specific custom discord bot."
+		url="https://github.com/iamgabrieltv/IamBot"
+	/>
+	<ProjectCard
+		title="iamgabrieltv.tk"
+		body="The Website you're on right now!"
+		url="https://github.com/iamgabrieltv/iamgabrieltv.tk"
+	/>
 </div>
