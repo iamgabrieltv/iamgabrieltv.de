c25e264fb4b172b755af9d9f9b9fdff1b813c67f
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-05-04 10:15:46 +0000

    First Concept

---
+++ src/lib/components/project-card.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+ export let title: string;
+ export let body: string;
+</script>
+
+<div class="card w-auto md:w-96 lg:w-96 bg-base-300 shadow-xl">
+	<div class="card-body">
+		<h2 class="card-title">{title}</h2>
+		<p>{body}</p>
+		<div class="card-actions justify-start">
+			<a href="https://github.com/iamgabrieltv/IamChat">
+				<button class="btn btn-primary">GitHub</button>
+			</a>
+		</div>
+	</div>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,13 +1,10 @@
+<script>
+ import ProjectCard from "$lib/components/project-card.svelte";
+</script>
+
 <h1 class="text-3xl font-bold mb-3">Projects</h1>
 
-<div class="card w-auto md:w-96 lg:w-96 bg-base-300 shadow-xl">
-	<div class="card-body">
-		<h2 class="card-title">IamChat</h2>
-		<p>A chat app made with SvelteKit and Firebase</p>
-		<div class="card-actions justify-start">
-			<a href="https://github.com/iamgabrieltv/IamChat">
-				<button class="btn btn-primary">GitHub</button>
-			</a>
-		</div>
-	</div>
+<div class="grid gap-1 auto-cols-auto">
+<ProjectCard title="Hello World!" body="Lorem Ipsum Dolor Sit Amet." />
+<ProjectCard title="Another Card!" body="Wow! It works" />
 </div>
