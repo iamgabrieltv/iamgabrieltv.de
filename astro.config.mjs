03a5eff21a666d2c6d93b09ff4b63f3d111369cc
Gabriel Sarcevic <gabrielsarcevic1@gmail.com>
2024-11-20 22:48:34 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), svelte()],
+  output: "hybrid",
+  adapter: cloudflare(),
+});
